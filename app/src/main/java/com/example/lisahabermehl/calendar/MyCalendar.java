68537dcf6ab7d796aeac0e0fb662d78f6ccd1cdc
lisahabermehl <habermehl.lisa@gmail.com>
2017-06-09 23:02:30 +0200

    option to see TODO's for one day

--- app/src/main/java/com/example/lisahabermehl/calendar/MyCalendar.java
+++ app/src/main/java/com/example/lisahabermehl/calendar/MyCalendar.java
@@ -6,11 +6,22 @@ package com.example.lisahabermehl.calendar;
  * Source code: http://www.viralandroid.com/2015/11/android-calendarview-example.html
  */
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,7 +31,7 @@ public class MyCalendar extends AppCompatActivity {
     TextView dateDisplay;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_calendar);
 
@@ -32,7 +43,20 @@ public class MyCalendar extends AppCompatActivity {
             @Override
             public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
                 dateDisplay.setText("Date: " + i2 + " / " + i1 + " / " + i);
-//                go to a listview with all the things that have to be done on that day
+//                go to a listview with all the things that have to be done on that
+
+                // startActivity and send data
+                Intent newActivity = new Intent(getApplicationContext(), Settings.class);
+                Bundle extras = new Bundle();
+
+//                String test = "something something";
+
+                extras.putInt("callingActivity", i);
+                Log.d(String.valueOf(i), "THIS");
+                newActivity.putExtras(extras);
+                startActivity(newActivity);
+//                startActivity(new Intent(getApplicationContext(), Settings.class));
+
             }
         });
     }
--- app/src/main/java/com/example/lisahabermehl/calendar/Settings.java
+++ app/src/main/java/com/example/lisahabermehl/calendar/Settings.java
@@ -2,6 +2,8 @@ package com.example.lisahabermehl.calendar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 /**
  * Created by lisahabermehl on 09/06/2017.
@@ -9,9 +11,21 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Settings extends AppCompatActivity {
 
+    TextView settings;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        settings = (TextView) findViewById(R.id.settings);
+
+        Bundle extras = getIntent().getExtras();
+        String previousActivity = extras.getInt()
+        Log.d(String.valueOf(previousActivity), "THIS2");
+
+        settings.setText(previousActivity);
+
+
     }
 }
 
