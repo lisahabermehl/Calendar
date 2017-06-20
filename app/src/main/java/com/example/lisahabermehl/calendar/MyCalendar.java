f647f20e37c18e288352911424fc4d6c4afaf0f2
lisahabermehl <habermehl.lisa@gmail.com>
2017-06-20 19:39:41 +0200

    Adding works with add button, implemented the "search by day" as well
    So, it was working last time as well, but I kinda forgot that the first
    month = 0, so I was expecting my new activity to appear on a specific
    day, but in the end it appeared on that day but a month earlier, which
    I didn’t notice right away

--- app/src/main/java/com/example/lisahabermehl/calendar/MyCalendar.java
+++ app/src/main/java/com/example/lisahabermehl/calendar/MyCalendar.java
@@ -81,7 +81,7 @@ public class MyCalendar extends AppCompatActivity {
         time_end = 0;
         time_end_old = 0;
 
-        updateUI();
+        updateUI("no specific date");
 
     }
 
@@ -99,10 +99,82 @@ public class MyCalendar extends AppCompatActivity {
 
                 return true;
             case R.id.insert_event:
-                insertEvent();
+                LayoutInflater layoutInflater = LayoutInflater.from(this);
+                final View dialogView = layoutInflater.inflate(R.layout.alert_dialog_insert_event, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+                builder
+                        .setView(dialogView)
+                        .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MyCalendar.this, "Okeee dan", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setPositiveButton("INSERT EVENT", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                EditText editText = (EditText) dialogView.findViewById(R.id.event_title);
+                                String title = editText.getText().toString();
+
+                                DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.event_date_start);
+                                String day = String.valueOf(datePicker.getDayOfMonth());
+                                String month = String.valueOf(datePicker.getMonth()+1);
+                                String year = String.valueOf(datePicker.getYear());
+
+                                EditText startTime = (EditText) dialogView.findViewById(R.id.event_time_start);
+                                EditText endTime = (EditText) dialogView.findViewById(R.id.event_time_end);
+
+                                String startTim = startTime.getText().toString();
+                                String endTim = endTime.getText().toString();
+
+                                String start = year + "/" + month + "/" + day + " " +
+                                        startTim;
+                                String end = year + "/" + month + "/" + day + " " +
+                                        endTim;
+
+                                // startActivity to show activities on a specific day
+                                Intent newActivity = new Intent(getApplicationContext(), GoogleCalendarTest.class);
+                                Bundle extras = new Bundle();
+
+                                extras.putString("zero", "add");
+                                extras.putString("one", title);
+                                extras.putString("two", start);
+                                extras.putString("three", end);
+                                newActivity.putExtras(extras);
+                                startActivity(newActivity);
+                            }
+                        })
+                        .create()
+                        .show();
                 return true;
             case R.id.menu_day:
-                selectDate();
+                LayoutInflater layoutInflaterDay = LayoutInflater.from(this);
+                final View dialogViewDay = layoutInflaterDay.inflate(R.layout.calendar_main, null);
+
+                AlertDialog.Builder builderDay = new AlertDialog.Builder(this);
+                builderDay
+                        .setView(dialogViewDay)
+                        .setPositiveButton("SELECT DATE", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+                                String date = "2017-06-20";
+                                updateUI(date);
+
+                            }
+                        })
+                        .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .create()
+                        .show();
                 return true;
             case R.id.menu_calendar:
                 startActivity(new Intent(this, MyCalendar.class));
@@ -115,7 +187,9 @@ public class MyCalendar extends AppCompatActivity {
         }
     }
 
-    private void updateUI(){
+    private void updateUI(String specific_date){
+        listView = (ListView) findViewById(R.id.list_calendar);
+        myCalendarDbHelper = new MyCalendarDbHelper(this);
         SQLiteDatabase db = myCalendarDbHelper.getReadableDatabase();
         Cursor cursor = db.query(MyCalendarTable.CalendarEntry.TABLE,
                 new String[]{TaskTable.TaskEntry._ID,
@@ -128,101 +202,130 @@ public class MyCalendar extends AppCompatActivity {
 
         ArrayList<MyCalendarObject> calendarObjects = new ArrayList<>();
 
-        while (cursor.moveToNext()) {
-            int id = cursor.getColumnIndex(MyCalendarTable.CalendarEntry._ID);
-            int title = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_TITLE);
-            int date = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_DATE);
-            int start = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_START);
-            int end = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_END);
+        if (specific_date.equals("no specific date")) {
+            while (cursor.moveToNext()) {
+                int id = cursor.getColumnIndex(MyCalendarTable.CalendarEntry._ID);
+                int title = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_TITLE);
+                int date = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_DATE);
+                int start = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_START);
+                int end = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_END);
 
-            String title_string = cursor.getString(title);
-            String date_string = cursor.getString(date);
-            String start_string = cursor.getString(start);
-            String end_string = cursor.getString(end);
+                String title_string = cursor.getString(title);
+                String date_string = cursor.getString(date);
+                String start_string = cursor.getString(start);
+                String end_string = cursor.getString(end);
 //            Log.d("ID", cursor.getString(id));
 //            Log.d("ACTIVITY", cursor.getString(title));
 //            Log.d("DATE", cursor.getString(date));
 //            Log.d("START", cursor.getString(start));
 //            Log.d("END", cursor.getString(end));
 
-            MyCalendarObject to = new MyCalendarObject(title_string,
-                    date_string, start_string, end_string);
+                MyCalendarObject to = new MyCalendarObject(title_string,
+                        date_string, start_string, end_string);
 
-            String[] start_split = start_string.split(":");
-            start_hour = Integer.valueOf(start_split[0]);
-            start_mins = Integer.valueOf(start_split[1]);
-            time_start = (start_hour * 60) + start_mins;
+                String[] start_split = start_string.split(":");
+                start_hour = Integer.valueOf(start_split[0]);
+                start_mins = Integer.valueOf(start_split[1]);
+                time_start = (start_hour * 60) + start_mins;
 
-            String[] time_split_end = end_string.split(":");
-            end_hour = Integer.valueOf(time_split_end[0]);
-            end_mins = Integer.valueOf(time_split_end[1]);
-            time_end = (end_hour * 60) + end_mins;
+                String[] time_split_end = end_string.split(":");
+                end_hour = Integer.valueOf(time_split_end[0]);
+                end_mins = Integer.valueOf(time_split_end[1]);
+                time_end = (end_hour * 60) + end_mins;
 
-            time_gap = time_start - time_end_old;
-            Log.d("TIME END OLD", String.valueOf(time_end_old));
-            Log.d("TIME START", String.valueOf(time_start));
-            Log.d("TIME END", String.valueOf(time_end));
-            Log.d("TIME GAP", String.valueOf(time_gap));
-            time_gap_hour = time_gap / 60;
-            Log.d("TIME GAP HOUR", String.valueOf(time_gap_hour));
+                time_gap = time_start - time_end_old;
+                Log.d("TIME END OLD", String.valueOf(time_end_old));
+                Log.d("TIME START", String.valueOf(time_start));
+                Log.d("TIME END", String.valueOf(time_end));
+                Log.d("TIME GAP", String.valueOf(time_gap));
+                time_gap_hour = time_gap / 60;
+                Log.d("TIME GAP HOUR", String.valueOf(time_gap_hour));
 
-            date_new = date_string;
+                date_new = date_string;
 
-            Log.d("DATE NEW", date_new);
-            Log.d("DATE OLD", date_old);
+                Log.d("DATE NEW", date_new);
+                Log.d("DATE OLD", date_old);
 
-            if (date_new.equals(date_old)) {
-                if(time_gap > 120) {
-                    MyCalendarObject to2 = new MyCalendarObject("Big time gap", date_string,
-                            String.valueOf(time_gap), String.valueOf(time_gap_hour));
+                if (date_new.equals(date_old)) {
+                    if(time_gap > 120) {
+                        MyCalendarObject to2 = new MyCalendarObject("Big time gap", date_string,
+                                String.valueOf(time_gap), String.valueOf(time_gap_hour));
 
-                    Log.d("NEW IS OLD", "TIME GAP > 15");
+                        Log.d("NEW IS OLD", "TIME GAP > 15");
 
-                    time_end_old = time_end;
-                    date_old = date_new;
+                        time_end_old = time_end;
+                        date_old = date_new;
 
-                    calendarObjects.add(to2);
-                    calendarObjects.add(to);
+                        calendarObjects.add(to2);
+                        calendarObjects.add(to);
+                    }
+                    else {
+                        Log.d("NEW IS OLD", "TIME GAP < 15");
+
+                        MyCalendarObject to3 = new MyCalendarObject("Small time gap", date_string,
+                                String.valueOf(time_gap), String.valueOf(time_gap_hour));
+
+                        time_end_old = time_end;
+                        date_old = date_new;
+                        calendarObjects.add(to3);
+                        calendarObjects.add(to);
+                    }
                 }
                 else {
-                    Log.d("NEW IS OLD", "TIME GAP < 15");
+                    Log.d("NEW ISN'T OLD", "TIME GAP > 15");
+                    // calculate based on different date
 
-                    MyCalendarObject to3 = new MyCalendarObject("Small time gap", date_string,
-                            String.valueOf(time_gap), String.valueOf(time_gap_hour));
+                    int time_gap_evening = bedtime_start - time_end_old;
+                    int time_gap_evening_hour = time_gap_evening / 60;
+                    int time_gap_morning = time_start - bedtime_end;
+                    int time_gap_morning_hour = time_gap_morning / 60;
+
+                    MyCalendarObject to_gap_evening = new MyCalendarObject("Gap evening", date_string,
+                            String.valueOf(time_gap_evening), String.valueOf(time_gap_evening_hour));
+                    MyCalendarObject to_gap_morning = new MyCalendarObject("Gap morning", date_string,
+                            String.valueOf(time_gap_morning), String.valueOf(time_gap_morning_hour));
+
+                    Log.d("EVENING", String.valueOf(time_gap_evening));
+                    Log.d("MORNING", String.valueOf(time_gap_morning));
 
                     time_end_old = time_end;
                     date_old = date_new;
-                    calendarObjects.add(to3);
+                    calendarObjects.add(to_gap_evening);
+                    calendarObjects.add(to_gap_morning);
+                    calendarObjects.add(to);
+                }
+
+                Log.d("COUNT", Integer.toString(calendarObjects.size()));
+            }
+            Log.d("COUNT2", Integer.toString(calendarObjects.size()));
+
+        }
+        else {
+            String date_of_choice = "2017-06-20";
+
+
+            while (cursor.moveToNext()) {
+                int date = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_DATE);
+                String date_string = cursor.getString(date);
+                Log.d("Date string", date_string);
+
+
+                if (date_of_choice.equals(date_string)) {
+                    int id = cursor.getColumnIndex(MyCalendarTable.CalendarEntry._ID);
+                    int title = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_TITLE);
+                    int start = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_START);
+                    int end = cursor.getColumnIndex(MyCalendarTable.CalendarEntry.COL_CAL_END);
+
+                    String title_string = cursor.getString(title);
+                    String start_string = cursor.getString(start);
+                    String end_string = cursor.getString(end);
+
+                    MyCalendarObject to = new MyCalendarObject(title_string,
+                            date_string, start_string, end_string);
                     calendarObjects.add(to);
                 }
             }
-            else {
-                Log.d("NEW ISN'T OLD", "TIME GAP > 15");
-                // calculate based on different date
-
-                int time_gap_evening = bedtime_start - time_end_old;
-                int time_gap_evening_hour = time_gap_evening / 60;
-                int time_gap_morning = time_start - bedtime_end;
-                int time_gap_morning_hour = time_gap_morning / 60;
-
-                MyCalendarObject to_gap_evening = new MyCalendarObject("Gap evening", date_string,
-                        String.valueOf(time_gap_evening), String.valueOf(time_gap_evening_hour));
-                MyCalendarObject to_gap_morning = new MyCalendarObject("Gap morning", date_string,
-                        String.valueOf(time_gap_morning), String.valueOf(time_gap_morning_hour));
-
-                Log.d("EVENING", String.valueOf(time_gap_evening));
-                Log.d("MORNING", String.valueOf(time_gap_morning));
-
-                time_end_old = time_end;
-                date_old = date_new;
-                calendarObjects.add(to_gap_evening);
-                calendarObjects.add(to_gap_morning);
-                calendarObjects.add(to);
-            }
-
-            Log.d("COUNT", Integer.toString(calendarObjects.size()));
         }
-        Log.d("COUNT2", Integer.toString(calendarObjects.size()));
 
         if (myCalendarAdapter == null) {
             myCalendarAdapter = new MyCalendarAdapter(this, 0, calendarObjects);
@@ -251,70 +354,7 @@ public class MyCalendar extends AppCompatActivity {
 
     private void insertEvent() {
 
-        final TextView textView = new TextView(this);
-        textView.setText("Test test");
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder
-                .setView(textView)
-                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MyCalendar.this, "Okeee dan", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setPositiveButton("INSERT EVENT", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent = new Intent(getApplicationContext(), GoogleCalendarTest.class);
-                        Bundle extras = new Bundle();
-
-                        extras.putString("zero", "add");
-                        extras.putString("one", "New new new");
-                        extras.putString("two", "2017/07/02 21:00");
-                        extras.putString("three", "2017/07/02 22:00");
-
-                        intent.putExtras(extras);
-                        startActivity(intent);
-                    }
-                })
-                .create()
-                .show();
 
     }
 
-    private void selectDate() {
-        LayoutInflater layoutInflaterDay = LayoutInflater.from(this);
-        final View dialogViewDay = layoutInflaterDay.inflate(R.layout.calendar_main, null);
-
-        AlertDialog.Builder builderDay = new AlertDialog.Builder(this);
-        builderDay
-                .setView(dialogViewDay)
-                .setPositiveButton("SELECT DATE", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        calendarView = (CalendarView) findViewById(R.id.calendarView);
-
-                            String date = "2017-06-20";
-
-                            // startActivity to show activities on a specific day
-                            Intent newActivity = new Intent(getApplicationContext(), GoogleCalendarTest.class);
-                            Bundle extras = new Bundle();
-
-                            extras.putString("date", date);
-                            newActivity.putExtras(extras);
-                            startActivity(newActivity);
-
-                        }
-                })
-                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                })
-                .create()
-                .show();
-    }
 }
