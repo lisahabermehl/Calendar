6e8e6bd7f393198d206ecc1c10d17ca8bab7ccb7
lisahabermehl <habermehl.lisa@gmail.com>
2017-06-22 11:38:10 +0200

    Added an "intro screen"

--- app/src/main/java/com/example/lisahabermehl/calendar/LoginActivity.java
+++ app/src/main/java/com/example/lisahabermehl/calendar/LoginActivity.java
@@ -4,8 +4,12 @@ package com.example.lisahabermehl.calendar;
  * Everything that has to do with/is need to log a user in.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.widget.LinearLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,5 +17,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.intro);
+
+        AlphaAnimation animation = new AlphaAnimation(0.0f , 1.0f ) ;
+        animation.setFillAfter(true);
+        animation.setDuration(4000);
+
+        //apply the animation ( fade In ) to your LAyout
+        linearLayout.startAnimation(animation);
+
+        int secondsDelayed = 5;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+
+//        startActivity(new Intent(this, MainActivity.class));
     }
 }
--- app/src/main/java/com/example/lisahabermehl/calendar/MainActivity.java
+++ app/src/main/java/com/example/lisahabermehl/calendar/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.lisahabermehl.calendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.LinearLayout;
 
 /**
  * Created by lisahabermehl on 09/06/2017.
@@ -14,6 +17,10 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 
     // go to MyCalendar
